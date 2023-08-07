e6999890f823ef3b69a86442bc24889dd00ccbe8
AvniChauhan03 <136076288+AvniChauhan03@users.noreply.github.com>
2023-08-07 20:29:22 +0530

    Add files via upload

---
+++ metadata/contractAddress.js
@@ -0,0 +1,3 @@
+
+    export const nftAddress = "0x724db22da1c086822Bf44381a59316D4780db136"
+  
---
+++ metadata/nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+  {
+    name: "0",
+    description: "A dog playing on the beach.",
+    image: "QmbTio7D1yw6jX6s2voc28creVztEJ194rLpdxU2NrjgbJ",
+  },
+  {
+    name: "1",
+    description: "A dog playing on the beach.",
+    image: "QmW3SwU6evTz5D7wj5e3499jXs94FBqGogkzFsbHwjBCcK",
+  }, //change
+  {
+    name: "2",
+    description: "A dog playing on the beach.",
+    image: "QmRh7pNHxKxmQanfR9xppZpgMHEFrwpC3xyPWfTbYnFqhD",
+  },
+  {
+    name: "3",
+    description: "A dog playing on the beach.",
+    image: "QmW8XcaoYxykrQmDTQQKP2VTUtmJGMpqg379hMLSDN9ueJ",
+  },
+  {
+    name: "4",
+    description: "A dog playing on the beach.",
+    image: "QmYpV1wPT4c5DPRJgE7jE3d7srUexD4vr4FMwAN1N38rzV",
+  },
+];
+
+module.exports = nfts;
